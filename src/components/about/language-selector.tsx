da61caec1de341855a918548227d51908e56ca73
Marsboy02 <marsboy0619@gmail.com>
2025-04-20 15:07:10 +0900

    docs: 문서 수정

--- src/components/about/language-selector.tsx
+++ src/components/about/language-selector.tsx
@@ -37,7 +37,7 @@ export default function LanguageSelector({ className }: { className?: string }) 
   };
 
   return (
-    <S.Select onValueChange={onSelectChange} defaultValue={isEn ? 'en' : isKo ? 'ko' : 'ja'}>
+    <S.Select onValueChange={onSelectChange} defaultValue={isKo ? 'ko' : isEn ? 'en' : 'ja'}>
       <S.SelectTrigger className={cn('w-fit gap-2', className)}>
         <GlobeIcon className='size-3.5' />
         <S.SelectValue />
